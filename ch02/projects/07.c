0062f7542e6e69622d2d6508fe6d8d6a2991724a
devstdlib <154808377+devstdlib@users.noreply.github.com>
2023-12-26 17:39:03 +0300

    Create 07.c

---
+++ ch02/projects/07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int amount;
+    int twenty;
+    int ten;
+    int five;
+    int one;
+
+    printf("Enter a dollar amount: ");
+    scanf("%d", &amount);
+
+    twenty = amount / 20;
+    amount = amount - (twenty * 20);
+    ten = amount / 10;
+    amount = amount - (ten * 10);
+    five = amount / 5;
+    amount = amount - (five * 5);
+    one = amount / 1;
+
+    printf("$20 bills: %d\n", twenty);
+    printf("$10 bills: %d\n", ten);
+    printf("$5 bills: %d\n", five);
+    printf("$1 bill: %d\n", one);
+
+    return 0;
+}
