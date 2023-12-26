784b29fb589bd6ad504a7cff6d95636c39088edf
devstdlib <154808377+devstdlib@users.noreply.github.com>
2023-12-26 13:31:58 +0300

    exercises added

---
+++ ch02/exercises/02.c
@@ -0,0 +1,18 @@
+/* directives: #include                     */
+/******************************************** 
+* --statements--                            *
+* Parkinson's Law:\nWork expands so as to   *
+*  fill the tine                            *
+*  available for its completion             *
+********************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's Law:\nWork expands so as to ");
+    printf("fill the tine\n");
+    printf("available for its completion.\n");
+
+    return 0;
+}
---
+++ ch02/exercises/04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2, num3;
+    float num4, num5, num6;
+
+    printf("num1: %d\n", num1);
+    printf("num2: %d\n", num2);
+    printf("num3: %d\n", num3);
+    printf("num4: %f\n", num4);
+    printf("num5: %f\n", num5);
+    printf("num6: %f\n", num6);
+
+    return 0;
+}
