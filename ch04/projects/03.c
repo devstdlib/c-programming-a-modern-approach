d57eab72eeefd00f18ff8200c5c635acc7fc0c6b
devstdlib <154808377+devstdlib@users.noreply.github.com>
2023-12-28 15:21:56 +0300

    Create 03.c

---
+++ ch04/projects/03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int first_digit, second_digit, third_digit;
+
+    printf("Enter a three digit-number: ");
+    scanf("%1d%1d%1d", &first_digit, &second_digit, &third_digit);
+    printf("%d%d%d\n", third_digit, second_digit, first_digit);
+
+    return 0;
+}
